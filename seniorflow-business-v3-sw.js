a2264f73a9596fe3dfe1b6214bbf626942c08e8f
Mauro <conexion354@gmail.com>
2026-03-27 12:36:24 -0300

    Add dedicated service worker for SeniorFlow Business v3

---
+++ seniorflow-business-v3-sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'seniorflow-business-v3-20260327-1';
+const APP_SHELL = [
+  './seniorflow-business-v3.html',
+  './seniorflow-business-v3.webmanifest',
+  './style.css',
+  './firebase-config.js',
+  './app-operations-v2.js',
+  './admin-tools-v3.js',
+  './icons/icon-app.svg',
+  './img/app-icon.svg',
+  './img/company-logo-wide.svg'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type === 'opaque') return response;
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      }).catch(() => {
+        if (event.request.mode === 'navigate') return caches.match('./seniorflow-business-v3.html');
+        return caches.match('./icons/icon-app.svg');
+      });
+    })
+  );
+});
